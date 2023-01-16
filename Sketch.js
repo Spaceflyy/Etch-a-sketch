b35a18d62589d9b2687a48366e3ea6a0c71bfab3
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2023-01-16 19:18:13 +0000

    Add size changing and clear button

--- Sketch.js
+++ Sketch.js
@@ -1,12 +1,49 @@
-const container = document.querySelector("#cellContainer");
+createGrid();
+let currentSize = 16;
+const buttonSize = document.querySelector("button.size");
+buttonSize.addEventListener('click',newGrid);
+document.querySelector("button.clearGrid").addEventListener('click',clearGrid)
 
-for (let i =0; i <16*16; i++)
+
+function clearGrid()
 {
-    const cells = document.createElement("div");
-    cells.classList.add("grid-element");
-    cells.addEventListener('mouseover',()=>{cells.style.background = "black"});
-    cells.style.width = `${500/16}px`;
-    cells.style.height = `${500/16}px`;
-    container.appendChild(cells);
+    document.getElementById("cellContainer").innerHTML ="";
+    createGrid(currentSize);
 }
 
+
+function createGrid(size = 16)
+{
+    const container = document.querySelector("#cellContainer");
+    for(let j =0; j<size; j++)
+    {
+        for (let i =0; i <size; i++)
+        {
+            const cells = document.createElement("div");
+            cells.classList.add("grid-element");
+            cells.addEventListener('mouseover',()=>{cells.style.background = "black"});
+            cells.style.width = `${500/size}px`;
+            cells.style.height = `${500/size}px`;
+            container.appendChild(cells);
+    }
+    }
+}
+
+
+function newGrid(){
+    let valid = false;
+    while(!valid){
+        let gridSize = prompt("Enter the size of the new grid: ");
+        if((gridSize > 100) || (gridSize < 2))
+        {
+            alert("Enter a number between 2 and 100")
+        } else
+        {
+            currentSize = gridSize;
+            valid = true;
+            clearGrid();
+            createGrid(gridSize);
+        }
+    }
+
+}
