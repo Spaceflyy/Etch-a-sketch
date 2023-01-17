be113e22924feb1a122e996600b942fd968ccfb0
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2023-01-17 19:14:01 +0000

    Add eraser & canvas size slider

--- Sketch.js
+++ Sketch.js
@@ -1,9 +1,39 @@
-createGrid();
-let currentSize = 16;
-const buttonSize = document.querySelector("button.size");
-buttonSize.addEventListener('click',newGrid);
-document.querySelector("button.clearGrid").addEventListener('click',clearGrid)
+const slider = document.querySelector("input");
+const DEFAULT_SIZE = 16;
+const value = document.querySelector(".sliderValue");
 
+document.querySelector("button.clearGrid").addEventListener('click',clearGrid);
+document.querySelector("button.eraser").addEventListener('click', toggleEraser);
+
+let eraserToggle = false;
+let currentSize = DEFAULT_SIZE;
+
+slider.oninput = function ()
+{
+    value.textContent = this.value;
+    currentSize = this.value;
+    clearGrid();
+}
+
+value.textContent = slider.value;
+
+
+function toggleEraser()
+{
+    if(eraserToggle == false)
+    {
+        eraserToggle = true;
+    } else
+    {
+        eraserToggle = false;
+    }
+}
+
+createGrid(DEFAULT_SIZE);
+function setCurrentSize(newSize)
+{
+    currentSize = newSize;
+}
 
 function clearGrid()
 {
@@ -12,7 +42,7 @@ function clearGrid()
 }
 
 
-function createGrid(size = 16)
+function createGrid(size)
 {
     const container = document.querySelector("#cellContainer");
     for(let j =0; j<size; j++)
@@ -21,7 +51,16 @@ function createGrid(size = 16)
         {
             const cells = document.createElement("div");
             cells.classList.add("grid-element");
-            cells.addEventListener('mouseover',()=>{cells.style.background = "black"});
+            cells.addEventListener('mouseover',()=>{
+                if(!eraserToggle)
+                {
+                    cells.style.background = "black"
+                } else
+                {
+                    cells.style.background = "";
+                }
+            
+            });
             cells.style.width = `${500/size}px`;
             cells.style.height = `${500/size}px`;
             container.appendChild(cells);
@@ -36,14 +75,14 @@ function newGrid(){
         let gridSize = prompt("Enter the size of the new grid: ");
         if((gridSize > 100) || (gridSize < 2))
         {
-            alert("Enter a number between 2 and 100")
+            alert("Enter a number between 2 and 100");
         } else
         {
-            currentSize = gridSize;
+     
             valid = true;
+            setCurrentSize(gridSize);
             clearGrid();
-            createGrid(gridSize);
+  
         }
     }
-
 }
