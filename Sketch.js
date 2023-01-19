d74dc1863036ee6960d26c2f217ab05a4d63a706
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2023-01-19 15:03:15 +0000

    Add functionality for Random, shading and colour modes
    Add button switching
    
    Code refactoring

--- Sketch.js
+++ Sketch.js
@@ -1,15 +1,21 @@
+
 const slider = document.querySelector(".sliderContainer input");
 const DEFAULT_SIZE = 16;
 const value = document.querySelector(".sliderValue");
 const colourInput = document.querySelector(".settings #colourSelector");
 
-document.querySelector("button.clearGrid").addEventListener('click',clearGrid);
-const eraser  = document.querySelector("button.eraser");
-eraser.addEventListener('click', toggleEraser);
+const clearBtn = document.querySelector("button.clearGrid");
+const eraserBtn = document.querySelector("button.eraser");
+const colourBtn = document.querySelector("button.colourMode");
+const randomClrBtn = document.querySelector("button.randomColours");
+const shadeBtn = document.querySelector("button.shade");
+
+clearBtn.addEventListener('click',clearGrid);
 
 let eraserToggle = false;
 let currentSize = DEFAULT_SIZE;
-
+let currentMode;
+let opacity = 0;
 
 slider.oninput = function ()
 {
@@ -21,27 +27,55 @@ slider.oninput = function ()
 value.textContent = slider.value;
 
 
-function toggleEraser()
-{
-
-    if(eraserToggle == false)
-    {
-
-        eraserToggle = true;
-        eraser.classList.add("active");
-
-    } else
-    {
-        eraserToggle = false;
-        eraser.classList.remove("active");
-    }
-}
-
 createGrid(DEFAULT_SIZE);
 function setCurrentSize(newSize)
 {
     currentSize = newSize;
 }
+
+eraserBtn.onclick = () =>{setCurrentMode('eraser')};
+colourBtn.onclick = () => {setCurrentMode('colour')};
+randomClrBtn.onclick = () =>{setCurrentMode('random')};
+shadeBtn.onclick = () => {setCurrentMode('shade')};
+
+function setButtonActive(mode)
+{
+
+    if(mode ==='eraser')
+    {
+        shadeBtn.classList.remove('active');
+        randomClrBtn.classList.remove('active');
+        colourBtn.classList.remove('active');
+        eraserBtn.classList.add('active');
+    } else if(mode === 'colour')
+    {
+        shadeBtn.classList.remove('active');
+        eraserBtn.classList.remove('active');
+        randomClrBtn.classList.remove('active');
+        colourBtn.classList.add('active');
+    } else if (mode ==='random')
+    {
+        shadeBtn.classList.remove('active');
+        eraserBtn.classList.remove('active');
+        colourBtn.classList.remove('active');
+        randomClrBtn.classList.add('active');
+    } else
+    {
+        eraserBtn.classList.remove('active');
+        colourBtn.classList.remove('active');
+        randomClrBtn.classList.remove('active');
+        shadeBtn.classList.add('active');
+
+    }
+}
+
+function setCurrentMode(newmode)
+{
+    currentMode = newmode;
+    setButtonActive(newmode);
+}
+
+
 
 function clearGrid()
 {
@@ -59,11 +93,8 @@ function createGrid(size)
         {
             const cells = document.createElement("div");
             cells.classList.add("grid-element");
-            cells.addEventListener('mouseover',()=>{
-
-                eraserToggle ?  cells.style.background = "" : cells.style.background = colourInput.value;
-                          
-            });
+            cells.addEventListener('mouseover',changeColour);
+            cells.addEventListener('mousedown',changeColour);
             cells.style.width = `${500/size}px`;
             cells.style.height = `${500/size}px`;
             container.appendChild(cells);
@@ -71,21 +102,29 @@ function createGrid(size)
     }
 }
 
+let mouseDown = false;
+document.body.onmousedown = () =>{mouseDown = true};
+document.body.onmouseup = () => {mouseDown =false};
+function changeColour(e)
+{
+    if(e.type === 'mouseover' && !mouseDown) return; 
 
-function newGrid(){
-    let valid = false;
-    while(!valid){
-        let gridSize = prompt("Enter the size of the new grid: ");
-        if((gridSize > 100) || (gridSize < 2))
-        {
-            alert("Enter a number between 2 and 100");
-        } else
-        {
-     
-            valid = true;
-            setCurrentSize(gridSize);
-            clearGrid();
-  
-        }
+    if (currentMode === "eraser")
+    {
+        e.target.style.backgroundColor = "";
+    }else if(currentMode ==='colour')
+    {
+        e.target.style.backgroundColor = colourInput.value;
+    }else if(currentMode === 'random')
+    {
+        let r = Math.floor(Math.random()* 255);
+        let g = Math.floor(Math.random()* 255);
+        let b = Math.floor(Math.random()* 255);
+        e.target.style.backgroundColor = 'rgba(' +r+ ',' +g+ ',' +b+')';
+    } else
+    {
+        opacity+=0.003;
+        e.target.style.backgroundColor = 'rgba('+0+','+0+','+0+',' +opacity+')';
     }
+          
 }
